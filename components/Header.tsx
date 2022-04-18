419041b171a2c53c22967a79409ad16059e6344f
Felipe Cunha <felipe.cunha@compasso.com.br>
2022-04-18 14:56:58 -0300

    header design completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,31 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className='flex justify-between p-5 max-w-7xl mx-auto'>
+      <div className='flex items-center space-x-5'>
+        <Link href="/"> 
+          <div className='flex object-contain items-center'>
+            <img 
+              className="w-20"
+              src="/images/logo.png"
+              alt="logo"
+            />
+            <h1 className='text-4xl font-serif font-bold'>Avaregium</h1>
+          </div>
+        </Link>
+        <div className='hidden md:inline-flex items-center space-x-5'>
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>Follow</h3>
+        </div>
+      </div>
+      <div className='flex items-center space-x-5 text-green-600'>
+        <h3>Sign In</h3>
+        <h3 className='border px-4 py-1 rounded-full border-green-600'>Get Started</h3>
+      </div>
+    </header>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,26 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto'>
       <Head>
         <title>Avaregium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+
+      <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
+        <div className='px-10 space-y-5'>
+          <h1 className='text-6xl max-w-xl font-serif'>
+            <span className='underline decoration-black decoration-4'>Avaregium</span> is a place to write, read and connect
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers.
+          </h2>
+        </div>
+        <img className='hidden md:inline-flex h-32 lg:h-full' src="/images/m.png" alt="m logo" />
+      </div>
     </div>
   )
 }
