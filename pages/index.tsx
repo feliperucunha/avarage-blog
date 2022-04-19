a70513316df8aba2068bae96d411af7bbceb19a3
Felipe Cunha <felipe.cunha@compasso.com.br>
2022-04-19 13:59:36 -0300

    sanity setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import { sanityClient, useCurrentUser } from '../sanity'
+import { Post } from '../typings'; 
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({ posts }: Props) {
   return (
     <div className='max-w-7xl mx-auto'>
       <Head>
@@ -28,4 +34,26 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = `
+    *[_type == "post"] {
+      _id,
+      title,
+      slug,
+      author -> {
+        name,
+        image
+      },
+      description,
+      mainImage,
+      body
+    }
+  `;
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    }
+  }
+};
